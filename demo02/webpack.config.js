cf1269169f7561cda71622b1b092ca3346e46c82
qinzhiwei <qinzhiwei@intgg.com>
2018-01-04 18:03:36 +0800

    demo01 html编译打包

--- demo02/webpack.config.js
+++ demo02/webpack.config.js
@@ -3,13 +3,23 @@
  */
 
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    context: path.resolve(__dirname), //定义上下文执行环境。 定义为当前目录
     entry: {
-        main: path.resolve(__dirname, './src/js/main.js')
+        main: [path.resolve(__dirname, './src/js/main.js'), path.resolve(__dirname, './src/js/a.js')], //数组形式是将连个不相干的，没有依赖的文件打包到一个文件里面
+        index: path.resolve(__dirname, './src/js/index.js')
     },
     output: {
-        path: path.resolve(__dirname, './dist/js'),
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, './dist'), //指定生成文件的保存路径
+        filename: 'js/[name].[hash].js', // [name]保留entry的命名入口文件的名字, [id]序号的名字 0 ， 1 , [hash], [chunkhash]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.[hash].html', //指定生成文件的名称
+            template: 'index.html',  //定义生成html的模板
+            inject: 'head' //执行应用的脚本放到头部还是body里面 【body head】
+        })
+    ]
 }
--- notes/README.md
+++ notes/README.md
@@ -2,7 +2,18 @@
 
 1、webpack --config [option]   指定配置文件。默认情况下指向webpack.config.js
 
-    --process 显示打包的百分比
+    --progress 显示打包的百分比
     --display-module 显示打包的模块
     --colors 打印为彩色
     --display-reason 显示打包原因
+    --watch 监听页面代码变化，实时打包编译
+
+2、loaders 加载器
+
+    style-loader 将css嵌入页面内
+    css-loader   打包css
+    less-loader  编译less为css 
+
+3、plugins 插件    
+
+    html-webpack-plugin 打包编译html
